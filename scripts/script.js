858598ded2047c94e0c1f650676514072cb90cf0
Juls Lin <julianna.lin4@gmail.com>
2023-03-10 18:40:02 -0800

    define constant array called courselist and add 3 items in it

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const CourseList = [
+    {code: "ACIT 1620", name: "Web Fundamental Technologies"},
+    {code: "ACIT 1420", name: "Introduction to Systems Administration"},
+    {code: "ACIT 1630", name: "Database System"}
+]
+
