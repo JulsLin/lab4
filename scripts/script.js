5565f314a861bf97714fb82166fb0016e3702899
Juls Lin <julianna.lin4@gmail.com>
2023-03-15 19:11:02 -0700

    add user input and invalid input to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
-const CourseList = [
+const courselist = [
     {code: "ACIT 1620", name: "Web Fundamental Technologies"},
     {code: "ACIT 1420", name: "Introduction to Systems Administration"},
     {code: "ACIT 1630", name: "Database System"}
-]
+];
 
+let input = prompt("Please enter a 4-digit number: ");
+
+while ((isNaN(input)) + input.length !== 4) {
+    input = prompt("Invalid input. Please enter a 4-digit number.")
+}
